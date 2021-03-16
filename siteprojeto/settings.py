1bf5c3f283fbae10e2f2d68c68c9a4aa43b82c90
root <root@localhost.localdomain>
2021-03-15 22:24:24 -0300

    .

--- siteprojeto/settings.py
+++ siteprojeto/settings.py
@@ -126,3 +126,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 if os.getcwd() == "/app":
 	ALLOWED_HOSTS = ["logindjango-erick.herokuapp.com"]
 	DEBUG = True
+
+	BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+	STATICFILES_DIRS = (
+		os.path.join(BASE_DIR, 'static')
+	)
