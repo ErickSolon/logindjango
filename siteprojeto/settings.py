dca6e6a0da9f33b4ff76c76a2a31d9631e4200e1
root <root@localhost.localdomain>
2021-03-15 22:40:41 -0300

    .

--- siteprojeto/settings.py
+++ siteprojeto/settings.py
@@ -124,9 +124,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 if os.getcwd() == "/app":
-	import dj_database_url
-	db_from_env = dj_database_url.config(conn_max_age=500)
-	DATABASES['default'].update(db_from_env)
 	ALLOWED_HOSTS = ["logindjango-erick.herokuapp.com"]
 	DEBUG = True
 
